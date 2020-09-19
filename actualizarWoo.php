85ada68268ec7dc2e3c28f73e96784af165dbb74
Jimmy Coa <jimmycoa@gmail.com>
2020-09-19 19:38:30 -0300

    actualizando

---
+++ actualizarWoo.php
@@ -0,0 +1,87 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+use Automattic\WooCommerce\Client;
+
+// Conexión WooCommerce API destino
+// ================================
+$url_API_woo = 'http://c1830079.ferozo.com/';
+$ck_API_woo = 'ck_9ce8ebcdc48a49dcf939cffab01d8f3fdd04a417';
+$cs_API_woo = 'cs_67c552e86a3dcd9ca40d878cb11131c45d679647';
+
+$woocommerce = new Client(
+    $url_API_woo,
+    $ck_API_woo,
+    $cs_API_woo,
+    ['version' => 'wc/v3']
+);
+// ================================
+
+
+// Conexión API origen
+// ===================
+$url_API="http://localhost/ClientephpWoocomerce/DataJson/DataOrigen.json";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url_API);
+
+echo "➜ Obteniendo datos origen ... \n";
+$items_origin = curl_exec($ch);
+curl_close($ch);
+
+if ( ! $items_origin ) {
+    exit('❗Error en API origen');
+}
+// ===================
+
+
+// Obtenemos datos de la API de origen
+$items_origin = json_decode($items_origin, true);
+
+// formamos el parámetro de lista de SKUs a actualizar
+$param_sku ='';
+foreach ($items_origin as $item){
+    $param_sku .= $item['sku'] . ',';
+}
+
+echo "➜ Obteniendo los ids de los productos... \n";
+// Obtenemos todos los productos de la lista de SKUs
+$products = $woocommerce->get('products/?sku='. $param_sku);
+
+// Construimos la data en base a los productos recuperados
+$item_data = [];
+foreach($products as $product){
+
+    // Filtramos el array de origen por sku
+    $sku = $product->sku;
+    $search_item = array_filter($items_origin, function($item) use($sku) {
+        return $item['sku'] == $sku;
+    });
+    $search_item = reset($search_item);
+
+    // Formamos el array a actualizar
+    $item_data[] = [
+        'id' => $product->id,
+        'regular_price' => $search_item['price'],
+        'stock_quantity' => $search_item['qty'],
+    ];
+
+}
+
+// Construimos información a actualizar en lotes
+$data = [
+    'update' => $item_data,
+];
+
+echo "➜ Actualización en lote ... \n";
+// Actualización en lotes
+$result = $woocommerce->post('products/batch', $data);
+
+if (! $result) {
+    echo("❗Error al actualizar productos \n");
+} else {
+    print("✔ Productos actualizados correctamente \n");
+}
+
+
--- index.php
+++ index.php
@@ -1,101 +1,22 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+<?php
 
-Jimmy Coa
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-      
-        <?php
-        
-        
-       require __DIR__ . '/vendor/autoload.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-       use Automattic\WooCommerce\Client;
+require __DIR__ . '/vendor/autoload.php';
+
+use Automattic\WooCommerce\Client;
 
 $woocommerce = new Client(
-    'http://c1830079.ferozo.com', 
-    'ck_f071a20f6af2ddf679445b736a8d08771c980ffb', 
-    'cs_bc71db83ce8ab7574a78298e733f3cb148e51d62',
+    'http://c1830079.ferozo.com/', 
+    'ck_9ce8ebcdc48a49dcf939cffab01d8f3fdd04a417', 
+    'cs_67c552e86a3dcd9ca40d878cb11131c45d679647',
     [
         'version' => 'wc/v3',
     ]
 );
-// ================================
-/*
-   $data = [
-    'regular_price' => '22.57'
-];
 
-print_r($woocommerce->put('products/9494', $data));
-*/
- 
-// ================================
-$url_API="http://localhost/ClientephpWoocomerce/DataJson/DataOrigen.json";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url_API);
-
-echo "➜ Obteniendo datos origen ... \n";
-$items_origin = curl_exec($ch);
-curl_close($ch);
-
-if ( ! $items_origin ) {
-    exit('❗Error en API origen');
-}
-// ===================
-$items_origin = json_decode($items_origin, true); 
-// ===================
-$param_sku ='';
-foreach ($items_origin as $item){
-    $param_sku .= $item['sku'] . ',';
-}
-
-echo "➜ Obteniendo los ids de los productos... \n";
-// ===================
-$products = $woocommerce->get('products/?sku='. $param_sku);
-// ===================
-$item_data = [];
-foreach($products as $product){
-
-    $sku = $product->sku;
-    $search_item = array_filter($items_origin, function($item) use($sku) {
-        return $item['sku'] == $sku;
-    });
-    $search_item = reset($search_item);
-
-// ===================   
-    $item_data[] = [
-        'id' => $product->id,
-        'regular_price' => $search_item['price'],
-        'stock_quantity' => $search_item['qty'],
-    ];
-
-}
-
-// ===================
-$data = [
-    'update' => $item_data,
-];
-
-echo "➜ Actualización en lote ... \n";
-// ===================
-$result = $woocommerce->post('products/batch', $data);
-
-if (! $result) {
-    echo("❗Error al actualizar productos \n");
-} else {
-    print("✔ Productos actualizados correctamente \n");
-}
-        ?>
-    
-    </body>
-</html>
+print_r($woocommerce->get('orders'));
