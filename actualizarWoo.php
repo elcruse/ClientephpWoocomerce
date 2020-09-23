10c9c434223360f3495b0040c16851849814db32
Jimmy Coa <jimmycoa@gmail.com>
2020-09-22 21:58:30 -0300

    actualizacion 3

--- actualizarWoo.php
+++ actualizarWoo.php
@@ -15,73 +15,17 @@ $woocommerce = new Client(
     $cs_API_woo,
     ['version' => 'wc/v3']
 );
-// ================================
 
-
-// Conexión API origen
-// ===================
-$url_API="http://localhost/ClientephpWoocomerce/DataJson/DataOrigen.json";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url_API);
-
-echo "➜ Obteniendo datos origen ... \n";
-$items_origin = curl_exec($ch);
-curl_close($ch);
-
-if ( ! $items_origin ) {
-    exit('❗Error en API origen');
-}
-// ===================
-
-
-// Obtenemos datos de la API de origen
-$items_origin = json_decode($items_origin, true);
-
-// formamos el parámetro de lista de SKUs a actualizar
-$param_sku ='';
-foreach ($items_origin as $item){
-    $param_sku .= $item['sku'] . ',';
-}
-
-echo "➜ Obteniendo los ids de los productos... \n";
-// Obtenemos todos los productos de la lista de SKUs
-$products = $woocommerce->get('products/?sku='.$param_sku);
-
-// Construimos la data en base a los productos recuperados
-$item_data = [];
-foreach($products as $product){
-
-    // Filtramos el array de origen por sku
-    $sku = $product->sku;
-    $search_item = array_filter($items_origin, function($item) use($sku) {
-        return $item['sku'] == $sku;
-    });
-    $search_item = reset($search_item);
-
-    // Formamos el array a actualizar
-    $item_data[] = [
-        'id' => $product->id,
-        'regular_price' => $search_item['price'],
-        'stock_quantity' => $search_item['qty'],
-    ];
-
-}
-
-// Construimos información a actualizar en lotes
 $data = [
-    'update' => $item_data,
+    'regular_price' => '77.77'
+  
 ];
 
-echo "➜ Actualización en lote ... \n";
-// Actualización en lotes
-$result = $woocommerce->post('products/batch', $data);
+print_r($woocommerce->put('products/9519', $data));
 
-if (! $result) {
-    echo("❗Error al actualizar productos \n");
-} else {
-    print("✔ Productos actualizados correctamente \n");
-}
+$data = [
+    'stock_quantity'=> '95'
+  
+];
 
-
+print_r($woocommerce->put('products/9519', $data));
